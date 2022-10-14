0eee150803d4c9b818a1ea8369603c49f92743c7
Markus Sample <markussample@gmail.com>
2022-10-13 19:52:26 -0700

    campground details activity

---
+++ app/src/main/java/com/example/a3717project/CamgroundDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.example.a3717project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CamgroundDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camground_details);
+    }
+}
