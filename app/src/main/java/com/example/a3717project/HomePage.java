b0de8f53d7a6d7b4bdbe35813f92e6282e359d5f
Markus Sample <markussample@gmail.com>
2022-11-04 14:56:36 -0700

    Add maps view

--- app/src/main/java/com/example/a3717project/HomePage.java
+++ app/src/main/java/com/example/a3717project/HomePage.java
@@ -4,32 +4,37 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class HomePage extends AppCompatActivity {
+import java.util.Objects;
+
+public class HomePage extends AppCompatActivity implements OnMapReadyCallback  {
+    SupportMapFragment mapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.maps);
+        Objects.requireNonNull(mapFragment).getMapAsync(this);
     }
 
-    public void OpenProfileActivity(View view) {
-        Intent intent = new Intent(this, ProfileActivity.class);
-        startActivity(intent);
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        LatLng comp3717Lecture = new LatLng(49.25010954461797, -123.00275621174804);
+        googleMap.addMarker(new MarkerOptions().position(comp3717Lecture).title("COMP 3717 Lecture"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(comp3717Lecture, 15));
     }
 
-    public void OpenCampgroundDetails(View view) {
-        Intent intent = new Intent(this, CamgroundDetailsActivity.class);
-        startActivity(intent);
-    }
 
-    public void OpenHistoryActivity(View view) {
-        Intent intent = new Intent(this, HistoryActivity.class);
-        startActivity(intent);
-    }
 
-    public void Back(View view) {
-        onBackPressed();
-    }
+
 }
